bc2796d76a8e0ae9b3cf202a108bed71518d27e0
Steven Tang <18170455+steventango@users.noreply.github.com>
2021-11-19 22:07:48 -0700

    Use 300 dpi

--- housecounter.py
+++ housecounter.py
@@ -77,7 +77,7 @@ def main():
 
     plt.title('Map')
     plt.suptitle(f'House Count: {len(house_contours)}')
-    plt.savefig('out.png', dpi=900)
+    plt.savefig('out.png', dpi=300)
 
 
 if __name__ == '__main__':
